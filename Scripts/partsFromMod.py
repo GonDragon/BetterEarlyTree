9d77dcfae0bc27f1bb50a205a5bb5de27db04b60
Gonzalo Romero <gondragon13@gmail.com>
2021-01-25 03:13:49 -0300

    part parser script almost finished

--- Scripts/partsFromMod.py
+++ Scripts/partsFromMod.py
@@ -1,19 +1,50 @@
 import os
 import sys
-import pathlib
-
-moddir = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
-
-parts = {}
-
-# r = root, d = directories, f = files
-for r, d, f in os.walk(moddir):
-    for file in f:
-        if file.endswith(".cfg"):
-            parts[file] = os.path.join(r, file)
+import re
 
 
-for k, v in parts.items():
-    print('{0} : {1}'.format(k, v))
-    with open(v, 'r') as cfgFile:
-        print(' '.join(cfgFile.readlines()))
+def run():
+    moddir = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
+    modname = os.path.basename(moddir)
+
+    parts = {}
+
+    # r = root, d = directories, f = files
+    for r, _, f in os.walk(moddir):
+        for file in f:
+            if file.endswith(".cfg"):
+                parts[file] = os.path.join(r, file)
+
+    regex_modules = re.compile(r'([A-Z]+\n\{.*\})', flags=re.DOTALL)
+
+    modules = []
+    for _, v in parts.items():
+        with open(v, 'r', encoding='utf-8', errors='ignore') as cfgFile:
+            fileContent = ''.join(cfgFile.readlines())
+            raw_modules = regex_modules.findall(fileContent)
+            for raw_module in raw_modules:
+                modules.append(Ksp_Module(raw_module))
+
+    for module in modules:
+        if 'module' in module.attributes and module.attributes['module'] == 'Part':
+            print('Part: {0}, TechRequired: {1}, Mod: {2}'.format(
+                module.attributes['name'], module.attributes['TechRequired'], modname))
+
+
+class Ksp_Module:
+    _REGEX_MODULES = re.compile(r'([A-Z]+)\s+\{(.*)\}', flags=re.DOTALL)
+    _REGEX_ATTRIBUTES = re.compile(r'([A-Za-z]+)\s=\s(.*)')
+
+    def __init__(self, string):
+        modules = self._REGEX_MODULES.search(string)
+        self.name = modules.group(1)
+        self.attributes = {}
+        self.content = modules.group(2)
+
+        regex_attributes = self._REGEX_ATTRIBUTES.findall(modules.group(2))
+        for k, v in regex_attributes:
+            if not k in self.attributes:
+                self.attributes[k] = v
+
+
+run()
