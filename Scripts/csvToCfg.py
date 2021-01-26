98d029fa6d3b9338e30b3886173131077f70d187
Gonzalo Romero <gondragon13@gmail.com>
2021-01-26 03:34:46 -0300

    Script to create patches and start with squad

--- Scripts/csvToCfg.py
+++ Scripts/csvToCfg.py
@@ -5,30 +5,36 @@ import os
 import csv
 
 
-DEF_TEMPLATE = "@PART[{0}]:NEEDS[{1}]:AFTER[BetterEarlyTree]\n{{\n   @TechRequired = {2}\n}}"
+DEF_TEMPLATE = "@PART[{0}]:NEEDS[{1}]:AFTER[BetterEarlyTree]\n{{\n   @TechRequired = {2}\n   {3}\n}}"
 
 
 def main():
 
-    # dic[mod] = [{techRequired : [part1, part2, ...]}, ...]
+    # dic[mod] = [{techRequired : [(part1, price1), (part2, price2), ...]}, ...]
     parts_from_mod = {}
 
     with open('newPartArrangement.csv', "r") as documento:
         parts = csv.reader(documento, delimiter=',', quotechar='"')
 
         next(parts, None)  # skip the headers
-        for name, tech, mod in parts:
+        for name, tech, mod, price in parts:
             if not mod in parts_from_mod:
                 parts_from_mod[mod] = {}
             if not tech in parts_from_mod[mod]:
                 parts_from_mod[mod][tech] = []
-            parts_from_mod[mod][tech].append(name)
+            parts_from_mod[mod][tech].append((name, price))
 
     for mod in parts_from_mod:
         with open('_output/patches/BET-{0}.cfg'.format(mod), "w", newline='') as outputCfg:
-            for tech, names in parts_from_mod[mod].items():
-                outputCfg.write(DEF_TEMPLATE.format(
-                    '|'.join(names), mod, tech) + '\n\n')
+            outputCfg.write('// This section is Script-generated\n\n')
+            for tech, parts in parts_from_mod[mod].items():
+                for part in parts:
+                    name, price = part
+                    final_price = ''
+                    if price.isnumeric():
+                        final_price = '@entryCost = ' + price
+                    outputCfg.write(DEF_TEMPLATE.format(
+                        name, mod, tech, final_price) + '\n\n')
 
 
 if __name__ == "__main__":
